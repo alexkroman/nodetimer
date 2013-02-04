5fcd1b6f11fed0ddb0c9963fa6f1f4bbec84eae7
Alex Kroman <alex@newrelic.com>
2013-02-03 16:58:00 -0800

    rip out some experimental code

--- config/config.js
+++ config/config.js
@@ -5,26 +5,11 @@ module.exports = {
         name: 'Node timer'
       },
       db: 'mongodb://localhost/noobjs_dev',
-      facebook: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/facebook/callback"
-      },
-      twitter: {
-          clientID: "CONSUMER_KEY"
-        , clientSecret: "CONSUMER_SECRET"
-        , callbackURL: "http://localhost:3000/auth/twitter/callback"
-      },
       github: {
           clientID: 'APP_ID'
         , clientSecret: 'APP_SECRET'
         , callbackURL: 'http://localhost:3000/auth/github/callback'
       },
-      google: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/google/callback"
-      }
     }
   , test: {
 
--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,7 @@
-
 var mongoose = require('mongoose')
   , LocalStrategy = require('passport-local').Strategy
-  , TwitterStrategy = require('passport-twitter').Strategy
-  , FacebookStrategy = require('passport-facebook').Strategy
   , GitHubStrategy = require('passport-github').Strategy
-  , GoogleStrategy = require('passport-google-oauth').Strategy
   , User = mongoose.model('User')
-
 
 module.exports = function (passport, config) {
   // require('./initializer')
@@ -41,63 +36,6 @@ module.exports = function (passport, config) {
     }
   ))
 
-  // use twitter strategy
-  passport.use(new TwitterStrategy({
-        consumerKey: config.twitter.clientID
-      , consumerSecret: config.twitter.clientSecret
-      , callbackURL: config.twitter.callbackURL
-    },
-    function(token, tokenSecret, profile, done) {
-      User.findOne({ 'twitter.id': profile.id }, function (err, user) {
-        if (err) { return done(err) }
-        if (!user) {
-          user = new User({
-              name: profile.displayName
-            , username: profile.username
-            , provider: 'twitter'
-            , twitter: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        }
-        else {
-          return done(err, user)
-        }
-      })
-    }
-  ))
-
-  // use facebook strategy
-  passport.use(new FacebookStrategy({
-        clientID: config.facebook.clientID
-      , clientSecret: config.facebook.clientSecret
-      , callbackURL: config.facebook.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({ 'facebook.id': profile.id }, function (err, user) {
-        if (err) { return done(err) }
-        if (!user) {
-          user = new User({
-              name: profile.displayName
-            , email: profile.emails[0].value
-            , username: profile.username
-            , provider: 'facebook'
-            , facebook: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        }
-        else {
-          return done(err, user)
-        }
-      })
-    }
-  ))
-
   // use github strategy
   passport.use(new GitHubStrategy({
       clientID: config.github.clientID,
@@ -125,30 +63,4 @@ module.exports = function (passport, config) {
     }
   ))
 
-  // use google strategy
-  passport.use(new GoogleStrategy({
-      consumerKey: config.google.clientID,
-      consumerSecret: config.google.clientSecret,
-      callbackURL: config.google.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({ 'google.id': profile.id }, function (err, user) {
-        if (!user) {
-          user = new User({
-              name: profile.displayName
-            , email: profile.emails[0].value
-            , username: profile.username
-            , provider: 'google'
-            , google: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        } else {
-          return done(err, user)
-        }
-      })
-    }
-  ));
 }
