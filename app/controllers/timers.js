3be267f52fe8623f58144c983417a552fe07b159
Alex Kroman <alex@newrelic.com>
2013-02-03 19:18:04 -0800

    design changes

--- app/controllers/timers.js
+++ app/controllers/timers.js
@@ -33,31 +33,22 @@ exports.index = function(req, res){
     return res.redirect('/login')
   }
   Timer
-    .find({"user": req.user, "endedAt": {"$gt": new Date() }})
-    .populate('user', 'username')
-    .sort({'endedAt': 1})
-    .limit(10)
-    .exec(function(err, started_timers) {
-      if (err) return res.render('500')
-      Timer
-        .find({"user": req.user, "endedAt": {"$lt": new Date() }})
-        .populate('user', 'username')
-        .sort({'endedAt': -1})
-        .limit(10)
-        .exec(function (err, ended_timers) {
-          Timer
-            .findOne({"user": req.user, "endedAt": {"$gt": new Date() }})
-            .exec(function (err, open_timer) {
-              res.render('timers/index', {
-                title: 'List of Timers'
-                , started_timers: started_timers
-                , ended_timers: ended_timers
-                , open_timer: open_timer
-                , timer: new Timer({})
-              })
-        })
-      });
+  .find({"user": req.user, "endedAt": {"$lt": new Date() }})
+  .populate('user', 'username')
+  .sort({'endedAt': -1})
+  .limit(10)
+  .exec(function (err, ended_timers) {
+    Timer
+    .findOne({"user": req.user, "endedAt": {"$gt": new Date() }})
+    .exec(function (err, open_timer) {
+      res.render('timers/index', {
+        title: 'List of Timers'
+        , ended_timers: ended_timers
+        , open_timer: open_timer
+        , timer: new Timer({})
+      })
     })
+  });
 }
 
 exports.timer = function(req, res, next, id){
