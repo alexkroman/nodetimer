91b7bd1996a164b90f1fa9af9d3e9098812ec358
Alex Kroman <alex@newrelic.com>
2013-02-16 17:12:15 -0800

    login updates

---
+++ spec/login.js
@@ -0,0 +1,6 @@
+it("should respond with 200", function(done) {
+  request("http://localhost:3000/login", function(error, response, body){
+    expect(body).toMatch(/login/);
+    done();
+  });
+});
