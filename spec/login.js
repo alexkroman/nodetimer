daee46cb3b99476d781589d398343d3c1ea23563
Alex Kroman <alex@newrelic.com>
2013-02-18 07:53:03 -0800

    update

--- spec/login.js
+++ spec/login.js
@@ -1,3 +1,6 @@
+server = require('../server.js')
+jasmine = require('jasmine-node')
+
 it("should respond with 200", function(done) {
   request("http://localhost:3000/login", function(error, response, body){
     expect(body).toMatch(/login/);
